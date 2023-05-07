d06be2c24f43593e5c9ad18664054443c3078263
Adam Luszcz <adam.luszcz20@gmail.com>
2023-05-07 22:47:14 +0200

    added basic game structure

---
+++ src/Game.h
@@ -0,0 +1,29 @@
+#ifndef ANGRYBIRD_GAME_H
+#define ANGRYBIRD_GAME_H
+#endif //ANGRYBIRD_GAME_H
+
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
+
+class Game {
+
+public:
+    Game();
+    ~Game();
+
+    void init(const char* title, int x, int y, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+
+    bool running() {
+        return isRunning;
+    };
+
+private:
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
